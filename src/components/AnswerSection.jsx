a26de3ae5217ecf2e9854c51f6d07436f7150853
huqinlong <huqingl@hotmail.com>
2023-03-23 16:11:06 +0800

    add style

--- src/components/AnswerSection.jsx
+++ src/components/AnswerSection.jsx
@@ -2,14 +2,12 @@ const AnswerSection = ({ storedValues }) => {
   return (
     <>
       <div className="answer-container">
+        <p className="start-message">this is a test</p>
         {storedValues.map((value, index) => {
           return (
             <div className="answer-section" key={index}>
-              <p className="start-message">this is a test</p>
               <p className="question">{value.question}</p>
-              <p className="answer">
-                {value.answer}
-              </p>
+              <p className="answer">{value.answer}</p>
             </div>
           );
         })}
--- src/components/FormSection.jsx
+++ src/components/FormSection.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Input } from "antd";
+import { Input, Button } from "antd";
 const { TextArea } = Input;
 const FormSection = ({ generateResponse }) => {
   const [newQuestion, setNewQuestion] = useState("");
@@ -12,12 +12,11 @@ const FormSection = ({ generateResponse }) => {
         value={newQuestion}
         onChange={(e) => setNewQuestion(e.target.value)}
       />
-      <button
+
+      <Button
         className="btn"
         onClick={() => generateResponse(newQuestion, setNewQuestion)}
-      >
-        Generate Response
-      </button>
+      />
     </div>
   );
 };
